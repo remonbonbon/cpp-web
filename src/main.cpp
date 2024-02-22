a68ef827722174881666bb018703aa09dbd4ca08
remonbonbon <remonbonbon@users.noreply.github.com>
2024-02-23 08:19:18 +0900

    Add thread

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,7 @@
+#include <chrono>
 #include <iostream>
 #include <sstream>
+#include <thread>
 
 #include <httplib.h>
 using Request = httplib::Request;
@@ -11,9 +13,16 @@ using json = nlohmann::json;
 int main(void) {
   httplib::Server svr;
 
-  svr.Get("/", [](const Request &req, Response &res) {
-    std::cout << req.method << " " << req.path << std::endl;
+  // Logger
+  svr.set_logger([](const Request &req, const Response &res) {
+    std::cout << res.status << " " << req.method << " " << req.path
+              << std::endl;
+  });
 
+  //-------------------------------------
+  // GET /
+  // Hello World!
+  svr.Get("/", [](const Request &req, Response &res) {
     std::stringstream html;
     html << "<h1>Hello World!</h1>";
 
@@ -26,6 +35,24 @@ int main(void) {
     res.set_content(html.str(), "text/html");
   });
 
+  //-------------------------------------
+  // GET /thread
+  // Tests threading
+  svr.Get("/thread", [](const Request &req, Response &res) {
+    std::thread th([&]() {
+      std::cout << "Sleeping for 3 second.." << std::endl;
+
+      std::this_thread::sleep_for(
+          std::chrono::milliseconds(3000)); // 3秒間スリープします
+
+      std::cout << "3 second elapsed.." << std::endl;
+    });
+    th.detach(); // スレッドを投げっぱなしにする
+
+    res.set_content("OK", "text/html");
+  });
+
+  //-------------------------------------
   std::cout << "listen http://localhost:8080/" << std::endl;
   svr.listen("0.0.0.0", 8080);
 }
